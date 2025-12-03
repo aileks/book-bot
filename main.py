9a4647b46c7788b49b540f9b43f9999518407759
Aaliyah Harper <aaliyah@aileks.dev>
2025-12-03 18:11:31 -0500

    get book contents

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+def get_book_text(filepath):
+    file_contents = None
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+
 def main():
-    print("Hello from book-bot!")
+    contents = get_book_text("./books/frankenstein.txt")
+    print(contents)
 
 
 if __name__ == "__main__":
