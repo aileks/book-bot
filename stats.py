1ea4ebb5f192c89dcf4ec2fc837b655780f4e05e
Aaliyah Harper <aaliyah@aileks.dev>
2025-12-03 18:38:43 -0500

    make it more readable pls

--- stats.py
+++ stats.py
@@ -4,5 +4,14 @@ def get_word_count(text: str) -> int:
 
 
 def get_char_count(text: str) -> dict[str, int]:
-    letters = [char.lower() for char in text if char.isalpha()]
-    return {letter: letters.count(letter) for letter in set(letters)}
+    char_counts = {}
+
+    for char in text:
+        if char.isalpha():
+            char = char.lower()
+            if char in char_counts:
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
+
+    return char_counts
